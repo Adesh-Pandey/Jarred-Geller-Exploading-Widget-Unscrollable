0357d7b0e4c791db34bb020bfd3277497be4b1d2
Adesh-Pandey <75557670+Adesh-Pandey@users.noreply.github.com>
2023-03-26 09:39:01 +0545

    made unscrollable

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 
   plugins: [react()],
-  base: "/Jarred-Geller-ExploadingWidget/",
+  base: "/Jarred-Geller-Exploading-Widget-Unscrollable/",
 
 })
 
